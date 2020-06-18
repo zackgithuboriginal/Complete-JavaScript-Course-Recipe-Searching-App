0bb9fb499f66d52c32ad03f8fdf34466192b2d6f
zackgithuboriginal <55746057+zackgithuboriginal@users.noreply.github.com>
2020-06-18 11:10:54 +0100

    Implemented pagination to display results

--- src/js/index.js
+++ src/js/index.js
@@ -39,4 +39,14 @@ const controlSearch = async () => {
 elements.searchForm.addEventListener("submit", e=> {
   e.preventDefault();
   controlSearch();
-})
+});
+
+elements.searchResPages.addEventListener('click', e => {
+  const btn = e.target.closest('.btn-inline');
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+    console.log(goToPage);
+  }
+});
